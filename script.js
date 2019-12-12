106ca6c5e79c76930d1a8cee9fd2bf5c6c7627d8
a.bohomil <a.bohomil@icecat.biz>
2019-12-12 17:38:31 +0200

    module

--- script.js
+++ script.js
@@ -1,59 +1,77 @@
-const ALLOWED_DISTANCE_PX = 100;
+const CONFIG = {
+    /*TODO:  ALLOWED_DEVIATION_PERCENT*/
+    ALLOWED_DEVIATION_PX: 100,
+    FOCUS_TIME_MS: 700,
+    SCROLL_FREQUENCY: 200,
+    TRACKED_ELEMENTS: ['#elem1', '#elem3', '#elem4'],
+};
 
-function send(data) {
-    console.log( 'Elements: ' + data + ' send!');
-    alert('Elements: ' + data + ' send!')
-}
 window.onload = function () {
-    let tracked;
-    let timer;
-    window.onscroll = throttle(function () {
-        clearInterval(timer);
-        tracked = getViewportElement();
-        console.log(tracked);
-        timer = setTimeout(() => {
-            if (tracked) send(tracked)
-        },1000)
-    }, 200);
+    TrackerModule(CONFIG);
+};
 
-    function getViewportElement() {
-        const container = document.querySelector('.container');
-        const elems = container.querySelectorAll('div');
+var TrackerModule = function TrackerModule(config) {
+    const elements = _getElementsBySelectors(config.TRACKED_ELEMENTS);
+    _init();
+    function _init() {
+        let timer;
+        window.onscroll = _throttle(function () {
+            clearTimeout(timer);
+            const result = _getViewportElements();
+            timer = setTimeout(() => {
+                _handleResult(result)
+            }, config.FOCUS_TIME_MS)
+        }, config.SCROLL_FREQUENCY);
+    }
+
+    function _getElementsBySelectors(selectors) {
+        const selector = selectors.join(',');
+        return document.querySelectorAll(selector);
+    }
+
+    function _getViewportElements() {
         const inView = [];
-        elems.forEach(function (elem, i) {
+        elements.forEach(function (elem) {
             var bounding = elem.getBoundingClientRect();
             var viewWidth = window.innerWidth || document.documentElement.clientWidth;
             var viewHeight = window.innerHeight || document.documentElement.clientHeight;
             if (
-                bounding.top + ALLOWED_DISTANCE_PX >= 0 &&
+                bounding.top + config.ALLOWED_DEVIATION_PX >= 0 &&
                 bounding.left >= 0 &&
                 bounding.right <= viewWidth &&
-                bounding.bottom - ALLOWED_DISTANCE_PX <= viewHeight
+                bounding.bottom - config.ALLOWED_DEVIATION_PX <= viewHeight
             ) {
                 inView.push(elem.id);
             }
         });
         return inView;
     }
-};
-function throttle(fn, threshhold, scope) {
-    threshhold || (threshhold = 250);
-    var last,
-        deferTimer;
-    return function () {
-        var context = scope || this;
-        var now = +new Date,
-            args = arguments;
-        if (last && now < last + threshhold) {
-            // hold on to it
-            clearTimeout(deferTimer);
-            deferTimer = setTimeout(function () {
+
+    function _handleResult(data) {
+        console.log('Elements: ' + data + ' send!');
+        alert('Elements: ' + data + ' send!')
+    }
+
+
+    function _throttle(fn, delay, scope) {
+        delay || (delay = 250);
+        let last,
+            deferTimer;
+        return function () {
+            const context = scope || this;
+            const now = +new Date,
+                args = arguments;
+            if (last && now < last + delay) {
+                clearTimeout(deferTimer);
+                deferTimer = setTimeout(function () {
+                    last = now;
+                    fn.apply(context, args);
+                }, delay);
+            } else {
                 last = now;
                 fn.apply(context, args);
-            }, threshhold);
-        } else {
-            last = now;
-            fn.apply(context, args);
-        }
-    };
-}
+            }
+        };
+    }
+};
+
